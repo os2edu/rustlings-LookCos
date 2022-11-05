550fd712b226fe070ac74a4196cb1afac1125e58
lookcos <lookcos@gmail.com>
2022-11-05 15:51:09 +0800

    update

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,7 +4,6 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
